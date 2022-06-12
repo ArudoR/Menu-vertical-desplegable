021bd4f7284ec8bc2832b85cd6845be29308b930
Aldo Mejia <aldonear@gmail.com>
2022-06-12 12:20:51 -0500

    Agregue una funcion a mi archivo js

---
+++ main.js
@@ -0,0 +1,8 @@
+let listElements = document.querySelectorAll('.list__button--click');
+
+listElements.forEach(listElement =>{
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+    })
+});
